86fe0f5d6af674481c695ca4bbb612606c4e14d9
zwq00000 <zwq00000@hotmail.com>
2015-08-22 09:53:47 +0800

    切换到 Develope 分支

---
+++ farmintell.mqttclient/src/androidTest/java/com/lingya/farmintell/ClientAdapterTest.java
@@ -0,0 +1,18 @@
+package com.lingya.farmintell;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by zwq00000 on 15-8-22.
+ */
+public class ClientAdapterTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+}
